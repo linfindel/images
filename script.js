57c3397fda59716aa827b7c2aea0e7dac48f0fdf
Nadir Technologies <116459177+Nadir-Software@users.noreply.github.com>
2023-10-07 12:25:02 +0100

    Add Vibrant.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const imageElement = document.getElementById("image");
+const navbar = document.getElementById("navbar");
 
 const buttons = document.getElementById("buttons");
 const button1 = document.getElementById("button1");
@@ -64,6 +65,36 @@ function uploadLink() {
 
     imageElement.src = url;
 
+    generateMaterialDesignPalette(url, (error, palette) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log("Material Design Palette:", palette);
+
+            // Set the background color of buttons with alpha 0.25
+            button1.style.backgroundColor = generateRGBA(palette.accent, 0.25);
+            button2.style.backgroundColor = generateRGBA(palette.accent, 0.25);
+
+            // Change background color on hover to alpha 0.5
+            button1.addEventListener("mouseover", () => {
+                button1.style.backgroundColor = generateRGBA(palette.accent, 0.5);
+            });
+            button1.addEventListener("mouseout", () => {
+                button1.style.backgroundColor = generateRGBA(palette.accent, 0.25);
+            });
+
+            button2.addEventListener("mouseover", () => {
+                button2.style.backgroundColor = generateRGBA(palette.accent, 0.5);
+            });
+            button2.addEventListener("mouseout", () => {
+                button2.style.backgroundColor = generateRGBA(palette.accent, 0.25);
+            });
+
+            document.getElementById("navbar").style.backgroundColor = generateRGBA(palette.accent, 0.25);
+            // You can use the palette to style your UI elements
+        }
+    });
+
     imageOptions();
 }
 
@@ -77,6 +108,36 @@ function uploadFile() {
             reader.onload = function (e) {
                 let fileURL = e.target.result;
                 imageElement.src = fileURL;
+
+                generateMaterialDesignPalette(fileURL, (error, palette) => {
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        console.log("Material Design Palette:", palette);           
+            
+                        // Set the background color of buttons with alpha 0.25
+                        button1.style.backgroundColor = generateRGBA(palette.accent, 0.25);
+                        button2.style.backgroundColor = generateRGBA(palette.accent, 0.25);
+            
+                        // Change background color on hover to alpha 0.5
+                        button1.addEventListener("mouseover", () => {
+                            button1.style.backgroundColor = generateRGBA(palette.accent, 0.5);
+                        });
+                        button1.addEventListener("mouseout", () => {
+                            button1.style.backgroundColor = generateRGBA(palette.accent, 0.25);
+                        });
+            
+                        button2.addEventListener("mouseover", () => {
+                            button2.style.backgroundColor = generateRGBA(palette.accent, 0.5);
+                        });
+                        button2.addEventListener("mouseout", () => {
+                            button2.style.backgroundColor = generateRGBA(palette.accent, 0.25);
+                        });
+            
+                        document.getElementById("navbar").style.backgroundColor = generateRGBA(palette.accent, 0.25);
+                        // You can use the palette to style your UI elements
+                    }
+                });
             };
             reader.readAsDataURL(file); // Read the file as a data URL
         }
@@ -84,4 +145,55 @@ function uploadFile() {
     input.click();
 
     imageOptions();
+}
+
+// Function to generate a Material Design color palette from an image URL
+function generateMaterialDesignPalette(imageURL, callback) {
+    // Create an image element to load the image
+    const img = new Image();
+    img.crossOrigin = "Anonymous"; // Enable cross-origin access to the image
+  
+    // Set up an event listener for when the image is loaded
+    img.onload = function () {
+        // Create a Vibrant.js object to extract colors from the image
+        const vibrant = new Vibrant(img);
+        const swatches = vibrant.swatches();
+  
+        // Check if swatches were successfully generated
+        if (swatches) {
+            // Extract Material Design color palette
+            const palette = {
+                primary: swatches.Vibrant.getHex(),
+                primaryDark: swatches.DarkVibrant.getHex(),
+                primaryLight: swatches.LightVibrant.getHex(),
+                accent: swatches.Muted.getHex(),
+            };
+  
+            // Execute the callback function with the generated palette
+            callback(null, palette);
+        }
+        
+        else {
+            // Error handling if swatches couldn't be generated
+            callback("Failed to generate swatches", null);
+        }
+    };
+  
+    // Set the image source to the provided URL
+    img.src = imageURL;
+}
+
+// Function to generate an RGBA value with a specified alpha
+function generateRGBA(hex, alpha) {
+    // Remove the "#" symbol if present
+    hex = hex.replace(/^#/, '');
+
+    // Parse the hex color to RGB components
+    const bigint = parseInt(hex, 16);
+    const red = (bigint >> 16) & 255;
+    const green = (bigint >> 8) & 255;
+    const blue = bigint & 255;
+
+    // Create the RGBA string
+    return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
 }
